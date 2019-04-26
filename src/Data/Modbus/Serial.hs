6f3ff25e116972b79144050117a574f0d53cbf07
Lupino <lmjubuntu@gmail.com>
2019-04-26 11:16:06 +0800

    use strict bytestring on serial communion

--- src/Data/Modbus/Serial.hs
+++ src/Data/Modbus/Serial.hs
@@ -10,11 +10,11 @@ import           Control.Concurrent.STM.TVar (TVar, readTVar, writeTVar)
 import           Control.Monad               (when)
 import           Control.Monad.STM           (atomically)
 import           Data.Binary                 (Binary (..))
-import qualified Data.ByteString             as B (ByteString, drop, length)
-import           Data.ByteString.Lazy        (ByteString, fromStrict, toStrict)
-import qualified Data.ByteString.Lazy        as LB (append, concat, drop,
-                                                    dropWhile, elem, head,
-                                                    takeWhile)
+import           Data.ByteString             (ByteString)
+import qualified Data.ByteString             as B (drop, dropWhile, elem, head,
+                                                   length, takeWhile)
+import           Data.ByteString.Lazy        (fromStrict, toStrict)
+import qualified Data.ByteString.Lazy        as LB (concat)
 import           Data.Modbus.Types           (Packet, decode, encode)
 import           System.Hardware.Serialport  (SerialPort, flush, recv, send)
 
@@ -23,43 +23,43 @@ recvPacket port bufTvar = do
   !r <- getPacketData bufTvar
   case r of
     Just p ->
-      case decode p of
+      case decode (fromStrict p) of
         Nothing -> recvPacket port bufTvar
         Just p0 -> return p0
     Nothing -> do
       bs <- recv port 20
-      setPacketData bufTvar $ fromStrict bs
+      setPacketData bufTvar bs
       recvPacket port bufTvar
 
 getPacketData :: TVar ByteString -> IO (Maybe ByteString)
 getPacketData bufTvar = atomically $ do
-  buf <- LB.dropWhile (/=0x3A) <$> readTVar bufTvar
+  buf <- B.dropWhile (/=0x3A) <$> readTVar bufTvar
   if hasPacket buf then do
-    writeTVar bufTvar $ LB.dropWhile (/=0x0D) buf
-    return $ Just $ removePrefix $ LB.takeWhile (/=0x0D) buf
+    writeTVar bufTvar $! B.dropWhile (/=0x0D) buf
+    return $ Just $ removePrefix $ B.takeWhile (/=0x0D) buf
   else return Nothing
 
   where hasPacket :: ByteString -> Bool
-        hasPacket = LB.elem 0x0A
+        hasPacket = B.elem 0x0A
 
 -- remove packet prefix :
 removePrefix :: ByteString -> ByteString
 removePrefix bs
-  | LB.head bs == 0x3A = removePrefix $ LB.drop 1 bs
-  | LB.elem 0x3A bs    = removePrefix $ LB.dropWhile (/=0x3A) bs
+  | B.head bs == 0x3A = removePrefix $ B.drop 1 bs
+  | B.elem 0x3A bs    = removePrefix $ B.dropWhile (/=0x3A) bs
   | otherwise          = bs
 
 setPacketData :: TVar ByteString -> ByteString -> IO ()
 setPacketData bufTvar bs = atomically $ do
   buf <- readTVar bufTvar
-  writeTVar bufTvar (LB.dropWhile (/=0x3A) $ buf `LB.append` bs)
+  writeTVar bufTvar $! buf <> bs
 
 sendPacket :: Binary a => SerialPort -> Packet a -> IO ()
 sendPacket port p = do
   sendBuffer port $ toStrict $ LB.concat [":", encode p, "\r\n"]
   flush port
 
-sendBuffer :: SerialPort -> B.ByteString -> IO ()
+sendBuffer :: SerialPort -> ByteString -> IO ()
 sendBuffer _ "" = pure ()
 sendBuffer port bs = do
   v <- send port bs
